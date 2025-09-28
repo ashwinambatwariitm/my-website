a840f29c93af54cfbbf5225f3b06c57554398b67
Ashvinambatwar <24f2009283@ds.study.iitm.ac.in>
2025-09-28 15:14:49 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Simple JavaScript to show an interactive message
+document.getElementById("clickBtn").addEventListener("click", function () {
+  document.getElementById("message").textContent = "🎉 You clicked the button!";
+});
